c2c2616a581f7fdfbae613037ca10525592f0259
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-13 13:33:24 +0200

    update main test file

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,9 +4,13 @@
 #include "PresidentialPardonForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
+//ShrubberyCreationForm: Required grades: sign 145, exec 137
+//RobotomyRequestForm: Required grades: sign 72, exec 45
+//PresidentialPardonForm: Required grades: sign 25, exec 5
+
 int main(void)
 {
-	Bureaucrat b1("Sergiu", 145);
+	Bureaucrat b1("Sergiu", 150);
 	try
 	{
 		RobotomyRequestForm RForm("RobotomyForm");
@@ -14,6 +18,10 @@ int main(void)
 		ShrubberyCreationForm SForm("ShrubberyForm");
 		b1.signForm(SForm);
 		b1.executeForm(SForm);
+		b1.signForm(PForm);
+		b1.executeForm(PForm);
+		b1.signForm(RForm);
+		b1.executeForm(RForm);
 		std::cout<<"SignGrade: "<<PForm.getSignGrade()<<std::endl;
 		std::cout<<"ExecGrade: "<<PForm.getExecGrade()<<std::endl;
 	}
