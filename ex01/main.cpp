8e09ef69d9ace470017f5139fd6f2b070e554e6e
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-13 20:04:56 +0200

    removed try catch block from the main func

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,15 +5,8 @@
 int main(void)
 {
 
-	Bureaucrat b1("Sergiu", 21);
+	Bureaucrat b1("Sergiu", 20);
 	Form form1("Document", 20, 130);
-
-	try	{
-		form1.beSigned(b1);
-	}
-	catch (std::exception &e) {
-		std::cout<<"\e[31m\e[7mError:"<<e.what()<<std::endl;
-	}
 
 	b1.signForm(form1);
 	std::cout<<b1;
